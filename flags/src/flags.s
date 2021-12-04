6341ba05b61926c2f54874a641889a8596ec04a5
doesthisusername <yfirestorm@gmail.com>
2021-12-04 11:56:52 +0100

    Change effective addressing syntax because I forgot how nasm works

--- flags/src/flags.s
+++ flags/src/flags.s
@@ -4,9 +4,9 @@ default rel
 %macro flag_test 2
 global %1 %+ _flag_test
 %1 %+ _flag_test:
-    mov     register_save[0x00], rdi
-    mov     register_save[0x08], rsi
-    mov     register_save[0x10], rdx
+    mov     [register_save + 0x00], rdi
+    mov     [register_save + 0x08], rsi
+    mov     [register_save + 0x10], rdx
     push    0x0000
     popf
     %1      %2
@@ -14,9 +14,9 @@ global %1 %+ _flag_test
     pop     rax
     push    0xFCFF
     popf
-    mov     rdi, register_save[0x00]
-    mov     rsi, register_save[0x08]
-    mov     rdx, register_save[0x10]
+    mov     rdi, [register_save + 0x00]
+    mov     rsi, [register_save + 0x08]
+    mov     rdx, [register_save + 0x10]
     %1      %2
     pushf
     pop     rcx
